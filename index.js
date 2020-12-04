2b1c21c872aa6e6d74b50114f0e49c355b26138f
Alternius <stevenallen291@gmail.com>
2020-12-03 22:25:44 -0500

    Moved map to bottom right and added coordinate system

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const panellum = document.getElementById('panellum');
 const mapEl = document.getElementById('map');
 
+const minX = 100;
+const minY = 258;
+const maxX = 32250;
+const maxY = 23000;
+const offsetY = 1200.636222910216;
+
 function initPanorama(images) {
   pannellum.viewer('panorama', {
       "type": "cubemap",
@@ -18,18 +24,29 @@ function initPanorama(images) {
 var map, marker, bounds;
 function initMap() {
   map = L.map('map', {
-      minZoom: 2.2,
+      minZoom: -50,
       maxBounds: bounds,
       maxBoundsViscosity: 1.0,
       crs: L.CRS.Simple
   });
-  map.on('click', function(e){
+
+  L.easyButton('<img src="images/expand.png">', function(btn, map) {
+    mapEl.setAttribute("style","width:30vw");
+    mapEl.setAttribute("style","height:50vh");
+    setTimeout(function(){ map.invalidateSize()}, 400);
+    $(window).trigger('resize');
+  }).addTo(map);
+
+  map.on('click', function(e) {
     marker = new L.marker(e.latlng).addTo(map);
+    let x = e.latlng.lng;
+    let y = maxY - e.latlng.lat - offsetY;
+    alert(x + " | " + y);
   });
   map.on('drag', function() {
       map.panInsideBounds(bounds, { animate: true });
   });
-  var bounds = [[0,0], [100,100]];
+  var bounds = [[minY, minX], [maxY, maxX]];
   var image = L.imageOverlay('images/i3JVqbS.png', bounds).addTo(map);
   map.fitBounds(bounds);
 }
@@ -42,6 +59,14 @@ function getImages(name) {
     images.push(path + "/panorama_" + i + ".png")
   }
   return images;
+}
+
+var location, coordinates;
+function getRandomLocation() {
+  let locations = ["capital_chill", "ba_sing_se", "fnc"]
+  let randLocation = locations[Math.floor(Math.random() * locations.length)];
+  console.log(randLocation);
+  return []
 }
 
 initMap();
